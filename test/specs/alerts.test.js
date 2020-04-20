35645213feb3a770ad83a148e7672bdef619bd33
Johnny1992 <evgeny.frontend@gmail.com>
2020-04-20 18:09:39 +0300

    added tests confirm message

--- test/specs/alerts.test.js
+++ test/specs/alerts.test.js
@@ -6,4 +6,25 @@ describe("JS Alerts", () => {
     internetPage.clickAlertButton(1);
     assert.equal("I am a JS Alert", browser.getAlertText());
   });
+
+  it("Should accept alert", () => {
+    browser.acceptAlert();
+    assert.equal(
+      "You successfuly clicked an alert",
+      internetPage.getResultText()
+    );
+  });
+
+  it("Should dismiss alert", () => {
+    internetPage.clickAlertButton(2);
+    browser.dismissAlert();
+    assert.equal("You clicked: Cancel", internetPage.getResultText());
+  });
+
+  it("Should accept alert", () => {
+    internetPage.clickAlertButton(2);
+    browser.acceptAlert();
+    assert.equal("You clicked: Ok", internetPage.getResultText());
+    browser.pause(1000);
+  });
 });
